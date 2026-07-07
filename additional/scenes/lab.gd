1c52b1b5c10385255e48c12018230c61b524447e
Enarior <70465902+Enarior@users.noreply.github.com>
2026-07-07 16:48:11 +0200

    Curve beach + replaced player by static cameras

---
+++ additional/scenes/lab.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var cameras : Array[Camera3D]
+
+var active_camera = 0
+
+func _process(delta):
+	if Input.is_action_pressed("switch_camera"):
+		if active_camera < cameras.size()-1:
+			cameras[active_camera].current = false
+			cameras[active_camera+1].current = true
+			active_camera += 1
+		else:
+			cameras[-1].current = false
+			cameras[0].current = true
+			active_camera = 0
+		pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,59 +1,154 @@
 extends CharacterBody3D
 
-@export_group("Camera")
-@export_range(0.0,1.0) var mouse_sensitivity = 0.25
+signal coin_collected
 
-@export_group("Movement")
-@export var move_speed := 8.0
-@export var acceleration := 30.0
-@export var rotation_speed := 10.0
+@export_subgroup("Components")
 
-@onready var _camera_pivot: Node3D = %CameraPivot
-@onready var _camera: Node3D = %Camera
-@onready var _character: Node3D = %Character
+@export_subgroup("Properties")
+@export var movement_speed = 250
+@export var jump_strength = 7
 
+var movement_velocity: Vector3
+var rotation_direction: float
+var gravity = 0
 
-var _camera_input_direction := Vector2.ZERO
-var _last_movement_direction := Vector3.BACK
+var previously_floored = false
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_click"):
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	if event.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+var jump_single = true
+var jump_double = true
 
-func _unhandled_input(event: InputEvent) -> void:
-	var is_camera_motion := (
-		event is InputEventMouseMotion &&
-		Input.mouse_mode == Input.MOUSE_MODE_CAPTURED
-	)
-	
-	if is_camera_motion:
-		_camera_input_direction = event.screen_relative * mouse_sensitivity
-		
+@onready var camera = %Camera3D
 
-func _physics_process(delta: float) -> void:
-	# CAMERA ROTATION
-	_camera_pivot.rotation.x += _camera_input_direction.y * delta
-	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, PI/16, PI/3)
-	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
-	_camera_input_direction = Vector2.ZERO
-	
-	# MOVEMENT INPUT
-	
-	var raw_direction := Input.get_vector("move_left","move_right", "move_forward", "move_back")
-	var forward := _camera.global_basis.z
-	var right := _camera.global_basis.x
-	
-	var move_direction := forward * raw_direction.y + right * raw_direction.x
-	move_direction.y = 0
-	move_direction = move_direction.normalized()
-	
-	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+@onready var particles_trail = $ParticlesTrail
+@onready var sound_footsteps = $SoundFootsteps
+@onready var model = $Character
+@onready var animation = $Character/AnimationPlayer
+
+# Functions
+
+func _physics_process(delta):
+
+	# Handle functions
+
+	handle_controls(delta)
+	handle_gravity(delta)
+
+	handle_effects(delta)
+
+	# Movement
+
+	var applied_velocity: Vector3
+
+	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
+	applied_velocity.y = -gravity
+
+	velocity = applied_velocity
 	move_and_slide()
-	
-	if move_direction.length() > 0.2:
-		_last_movement_direction = move_direction
 
-	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
-	_character.global_rotation.y = lerp_angle(_character.rotation.y, target_angle, rotation_speed * delta)
+	# Rotation
+
+	if Vector2(velocity.z, velocity.x).length() > 0:
+		rotation_direction = Vector2(velocity.z, velocity.x).angle()
+
+	rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
+
+	# Falling/respawning
+
+	if position.y < -10:
+		get_tree().reload_current_scene()
+
+	# Animation for scale (jumping and landing)
+
+	model.scale = model.scale.lerp(Vector3(1, 1, 1), delta * 10)
+
+	# Animation when landing
+
+	if is_on_floor() and gravity > 2 and !previously_floored:
+		model.scale = Vector3(1.25, 0.75, 1.25)
+		Audio.play("res://sounds/land.ogg")
+
+	previously_floored = is_on_floor()
+
+# Handle animation(s)
+
+func handle_effects(delta):
+
+	particles_trail.emitting = false
+	sound_footsteps.stream_paused = true
+
+	if is_on_floor():
+		var horizontal_velocity = Vector2(velocity.x, velocity.z)
+		var speed_factor = horizontal_velocity.length() / movement_speed / delta
+		if speed_factor > 0.05:
+			if animation.current_animation != "walk":
+				animation.play("walk", 0.1)
+
+			if speed_factor > 0.3:
+				sound_footsteps.stream_paused = false
+				sound_footsteps.pitch_scale = speed_factor
+
+			if speed_factor > 0.75:
+				particles_trail.emitting = true
+
+		elif animation.current_animation != "idle":
+			animation.play("idle", 0.1)
+			
+		if animation.current_animation == "walk":
+			animation.speed_scale = speed_factor
+		else:
+			animation.speed_scale = 1.0
+			
+	elif animation.current_animation != "jump":
+		animation.play("jump", 0.1)
+
+# Handle movement input
+
+func handle_controls(delta):
+
+	# Movement
+
+	var input := Vector3.ZERO
+
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+
+	input = input.rotated(Vector3.UP, camera.rotation.y)
+
+	if input.length() > 1:
+		input = input.normalized()
+
+	movement_velocity = input * movement_speed * delta
+
+	# Jumping
+
+	if Input.is_action_just_pressed("jump"):
+
+		if jump_single or jump_double:
+			jump()
+
+# Handle gravity
+
+func handle_gravity(delta):
+
+	gravity += 25 * delta
+
+	if gravity > 0 and is_on_floor():
+
+		jump_single = true
+		gravity = 0
+
+# Jumping
+
+func jump():
+
+	Audio.play("res://sounds/jump.ogg")
+
+	gravity = -jump_strength
+
+	model.scale = Vector3(0.5, 1.5, 0.5)
+
+	if jump_single:
+		jump_single = false;
+		jump_double = true;
+	else:
+		jump_double = false;
